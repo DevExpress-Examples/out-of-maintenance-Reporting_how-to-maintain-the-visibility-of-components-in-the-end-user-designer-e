bf12630efb3dd53cc6158fb550d7c5a23275d36c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HideComponentTray/Form1.cs
@@ -0,0 +1,38 @@
+﻿#region #References
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraReports.Design;
+using DevExpress.XtraReports.UserDesigner;
+// ...
+#endregion #References
+
+namespace HideComponentTray {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+#region #ComponentVisibility
+private void button1_Click(object sender, EventArgs e) {
+    // Create an End-User Designer form.
+    XRDesignForm designForm = new XRDesignForm();
+
+    // Handle the DesignPanelLoaded event.
+    designForm.DesignMdiController.DesignPanelLoaded +=
+        new DesignerLoadedEventHandler(DesignMdiController_DesignPanelLoaded);
+
+    // Load a report into the Designer.
+    designForm.OpenReport(new XtraReport1());
+
+    // Show the End-User Designer form, modally.
+    designForm.ShowDialog();
+}
+
+// List the components in the Report Explorer, instead of the Component Tray.
+void DesignMdiController_DesignPanelLoaded(object sender, DesignerLoadedEventArgs e) {
+    ((XRDesignPanel)sender).ComponentVisibility = ComponentVisibility.ComponentTray;
+}
+#endregion #ComponentVisibility
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to maintain the visibility of components in the End-User Designer
+
+
+<p>The following example demonstrates how to maintain the visibility of components (such as <strong>DataSet</strong> and <strong>DataAdapter</strong>) in the <a href="http://documentation.devexpress.com/#XtraReports/CustomDocument1198"><u>End-User Report Designer</u></a> for Windows Forms.</p><p>To hide a report's associated components together, or list them in the <a href="http://documentation.devexpress.com/#XtraReports/CustomDocument4258"><u>Report Explorer</u></a> or the <strong>Component Tray</strong> displayed at the bottom of the <strong>Design Panel</strong>, use the <a href="http://documentation.devexpress.com/#XtraReports/DevExpressXtraReportsUserDesignerXRDesignPanel_ComponentVisibilitytopic"><u>XRDesignPanel.ComponentVisibility</u></a> property.</p>
+
+<br/>
+
+
