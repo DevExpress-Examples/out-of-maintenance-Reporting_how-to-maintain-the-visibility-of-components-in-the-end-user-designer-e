a53c1a929056cafd6a5114e172061f8da03bd2c5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HideComponentTray/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing.Design;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraReports.UserDesigner;
+// ...
+
+namespace HideComponentTray {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Create an End-User Designer form.
+            XRDesignForm designForm = new XRDesignForm();
+
+            // Handle the DesignPanelLoaded event.
+            designForm.DesignMdiController.DesignPanelLoaded +=
+                new DesignerLoadedEventHandler(DesignMdiController_DesignPanelLoaded);
+
+            // Load a report into the Designer.
+            designForm.OpenReport(new XtraReport1());
+
+            // Show the End-User Designer form, modally.
+            designForm.ShowDialog();
+        }
+
+        void DesignMdiController_DesignPanelLoaded(object sender, DesignerLoadedEventArgs e) {
+            // Hide the Component Tray from the Design Panel.
+            ((XRDesignPanel)sender).ShowComponentTray = false;
+        }
+
+    }
+}
---
+++ VB/HideComponentTrayVB/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports System.Drawing.Design
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.XtraReports.UserDesigner
+' ...
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ' Create an End-User Designer form.
+        Dim designForm As New XRDesignForm()
+
+        ' Handle the DesignPanelLoaded event.
+        AddHandler designForm.DesignMdiController.DesignPanelLoaded, AddressOf DesignMdiController_DesignPanelLoaded
+
+        ' Load a report into the Designer.
+        designForm.OpenReport(New XtraReport1())
+
+        ' Show the End-User Designer form, modally.
+        designForm.ShowDialog()
+    End Sub
+
+    Private Sub DesignMdiController_DesignPanelLoaded(ByVal sender As Object, ByVal e As DesignerLoadedEventArgs)
+        ' Hide the Component Tray from the Design Panel.
+        CType(sender, XRDesignPanel).ShowComponentTray = False
+    End Sub
+End Class
